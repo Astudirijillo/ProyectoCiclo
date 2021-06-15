beecdd6182f616c4b24b2794fe2a6f1d500a655b
Cristobal Astudillo <labotsirc.1997@gmail.com>
2021-06-15 16:09:06 -0400

    Primer commit

---
+++ src/components/agregarProveedores.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "agregarProveedores"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+////////// RUTAS //////////
+import agregarProveedores from "./components/agregarProveedores";
 
 Vue.config.productionTip = false
 
+const routes = [
+    {path : "/agregarProveedores", component : agregarProveedores}
+]
+
+const router = new VueRouter({
+    routes,
+    mode: "history"
+});
+Vue.use(VueRouter)
+
 new Vue({
-  render: h => h(App),
+    router,
+    render: h => h(App),
 }).$mount('#app')
